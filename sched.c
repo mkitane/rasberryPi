bbbf99dfb5177e9df1d04c19c22618e32312603e
mkitane <mehdikitane@gmail.com>
2014-10-17 01:02:17 +0200

    Avancement sur partie4

--- sched.c
+++ sched.c
@@ -1,4 +1,8 @@
 #include "sched.h"
+static struct pcb_s* first_process = (struct pcb_s *)0;  
+static struct pcb_s* last_process = (struct pcb_s *)0;  
+static struct pcb_s* current_process = (struct pcb_s *)0;  
+
 
 void init_ctx(struct ctx_s* ctx, func_t f, unsigned int stack_size){
 
@@ -8,46 +12,38 @@ void init_ctx(struct ctx_s* ctx, func_t f, unsigned int stack_size){
 }
 
 
-void __attribute__ ((naked)) switch_to(struct ctx_s* ctx)
-{
-	//Sauvegarder contexte courant
-
-    //Enregistrer les registres
-    __asm("push {r0-r12}"); 
-	__asm("mov %0, sp" : "=r"((*current_ctx).sp) );
-	__asm("mov %0, lr" : "=r"((*current_ctx).lr) );
-
-	//Changer de contexte courant
-	current_ctx = ctx; 
-	
-	//Restaurer ce nouveau contexte
-
-	__asm("mov sp, %0" : : "r"((*current_ctx).sp));
-	__asm("mov lr, %0" : : "r"((*current_ctx).lr));
-    __asm("pop {r0-r12}"); 
-
-	//Sauter a ladresse de retour du contexte restaure
-	__asm("bx lr"); 	
-}
-
 
 void init_pcb(struct pcb_s* pcb, func_t f, void* args, unsigned int stack_size){
-	(*pcb).id_process = compteur; 
-	compteur++; 
-	(*pcb).state_process = PRET; 
-	(*pcb).adress = f; 
-	(*pcb).sp = ((int)phyAlloc_alloc(stack_size)) + stack_size;
+	static int compteur = 0; 
+	(*pcb).id_process = compteur++; 
+	(*pcb).state_process = NEW; 
+
+
+	(*pcb).function = f; 
 	(*pcb).args_f = args;
+
+
+	(*pcb).adress = (int*)&start_current_process;   //Car start_current_process() est appelée pour
+                                                    //lancer un processus (pour la première fois)
+	(*pcb).sp = ((int)phyAlloc_alloc(stack_size)) + stack_size;
 
 }
 int create_process(func_t f, void* args, unsigned int stack_size)
 {
 	struct pcb_s *pcb = (struct pcb_s*)phyAlloc_alloc(sizeof(struct pcb_s));
 
-	last_process->next_process = pcb; 
-	pcb->next_process = first_process;
-	last_process = pcb; 
-
+	if(!first_process)
+	{
+		first_process = pcb; 
+		last_process = pcb;
+        
+        first_process->next = last_process;
+        last_process->next = first_process;
+	}else{
+		last_process->next_process = pcb; 
+		pcb->next_process = first_process;
+		last_process = pcb; 
+	}
 	init_pcb(pcb,f,args, stack_size);
 
 	return pcb->id_process;
@@ -55,13 +51,62 @@ int create_process(func_t f, void* args, unsigned int stack_size)
 
 void start_current_process()
 {
-	current_process->state_process = EXECUTION ; 
-	current_process->adress(current_process->args_f); 
+	current_process->state_process = READY ; 
+	current_process->function(current_process->args_f);
+	current_process->state_process = TERMINATED;
+	ctx_switch();
 }
+
+void elect ()
+{
+    struct pcb_s* pcb = current_process;
+
+    //On trouve le suivant qui n'est pas TERMINATED
+    while(pcb->next->state_process != TERMINATED){
+        if (pcb->next->id_process == current_process->id_process) { break; }
+        pcb = pcb ->next;
+    }
+    
+    //On supprime ceux qui sont terminated
+    if(pcb->next->state_process == TERMINATED){  //Q4.9
+        struct pcb_s* temp = pcb-> next;
+        //throw it out of the list
+        pcb->next = pcb->next->next;
+        phyAlloc_free(temp,sizeof(struct pcb_s));
+    }
+    
+    
+    current_process = pcb->next;
+    
+}
+
+
 void start_sched()
 {
+    current_process->next = first_process;
 }
-void ctx_switch()
+
+
+
+void __attribute__ ((naked)) ctx_switch() //Current process non initialisé?
 {
+    
+    //Sauvegarder contexte courant
+    
+    //Enregistrer les registres
+    __asm("push {r0-r12}");
+    __asm("mov %0, sp" : "=r"(current_process->sp));
+    __asm("mov %0, lr" : "=r"(current_process->adress));
+    
+    //elect a new process
+    elect();
+    
+    //Restaurer ce nouveau contexte
+    __asm("mov sp, %0" : : "r"(current_process->sp));
+    __asm("mov lr, %0" : : "r"(current_process->adress));
+    __asm("pop {r0-r12}");
+    //Sauter a ladresse de retour du contexte restaure
+    __asm("bx lr");
+    
 }
 
--- sched.h
+++ sched.h
@@ -17,23 +17,20 @@ void __attribute__ ((naked)) switch_to(struct ctx_s* ctx);
 
 
 //Partie 4
-enum process_state_s {PRET, EXECUTION, ENDORMI, INIT, TERMINE};
+enum process_state_s {NEW, READY, RUNNING, WAITING, TERMINATED};
 struct pcb_s
 {
     unsigned int id_process;
     enum process_state_s state_process;
     unsigned int sp;
     unsigned int adress;
+
+    func_t function;
 	void* args_f;
 
     struct pcb_s* next_process;
 };
 
-
-static unsigned int compteur = 0; 
-static struct pcb_s* first_process; 
-static struct pcb_s* last_process; 
-static struct pcb_s* current_process; 
 
 
 void init_pcb(struct pcb_s* pcb, func_t f, void* args, unsigned int stack_size);
